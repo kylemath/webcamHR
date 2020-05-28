fcec6db5bb2c2ba0b6991404b408e31bf0abb132
Kyle Mathewson <kylemath@gmail.com>
2020-05-27 23:36:36 -0700

    initialize html and js file

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(390, 240)
+	capture = createCapture(VIDEO)
+	capture.size(320, 240);
+	capture.hide()
+}
+
+function draw() {
+	background(220)
+	image(capture, 0, 0, 320, 240);
+}
