cde86db805786f07e7760d5705e8cd96c7608088
Kyle Mathewson <kylemath@gmail.com>
2020-06-07 00:47:14 -0700

    added in facetracking instead of center ROI

--- sketch.js
+++ sketch.js
@@ -1,55 +1,89 @@
-let cam;
-let g;
-let w, h;
+let faceapi;
+let video;
+let detections;
 let allAvg = [];
-let fft;
 let dCount = 1;
+let trim = 20;
 
-function setup() {
-	createCanvas(320, 480)
-	pixelDensity(1)
-	h = height/2;
-	w = width;
-	cam = createCapture(VIDEO)
-	cam.size(w, h);
-	cam.hide()
-	g = createImage(w, h);
-	g.loadPixels();
-	background(0);
+// by default all options are set to true
+const detection_options = {
+    withLandmarks: true,
+    withDescriptors: false,
 }
 
-function draw() {
+function setup() {
+    createCanvas(320, 480);
+    pixelDensity(1);
 
-
-  cam.loadPixels();
-  for (let i = 0; i < cam.pixels.length; i += 4) {
-    g.pixels[i + 0] = 0;
-    g.pixels[i + 1] = cam.pixels[i + 1];
-    g.pixels[i + 2] = 0;
-    g.pixels[i + 3] = 255;
-  }
-  g.updatePixels();
-  image(cam, 0, 0, w, h);
-    
-  // cutout the center box and take average value  
-  cutout = g.get(w/2-50, h/2-50, 100, 100);
-  cutout.loadPixels();
-  let sumG = 0;
-  for (let i = 0; i < cutout.pixels.length; i += 4) {
-  	sumG += (cutout.pixels[i + 0] + cutout.pixels[i + 1] + cutout.pixels[i + 2]);
-  }
-  avgG = sumG / cutout.pixels.length;
-  allAvg.push(avgG);
-  image(cutout, 0, 0);
-  
-  // print avg luminance as point on lower section
-  stroke(255);
-  print(avgG)
-  plotPoint = map(avgG,25, 30, 240, 480);
-  point(dCount,plotPoint);
-  dCount += 1;
-  if (dCount > 320) {
-    dCount = 1;
+    h = height/2;
+    // load up your video
+    video = createCapture(VIDEO);
+    video.size(width, h);
+    video.hide();
+    // video.hide(); // Hide the video element, and just show the canvas
+    faceapi = ml5.faceApi(video, detection_options, modelReady);
+    textAlign(RIGHT);
+    g = createImage(width, h);
     background(0);
-  }
+}
+
+function modelReady() {
+    console.log('ready!')
+    console.log(faceapi)
+    faceapi.detect(gotResults)
+
+}
+
+function gotResults(err, result) {
+    detections = result;
+    image(video, 0,0, width, h)
+    if (detections) {
+        if (detections.length > 0) {
+            // console.log(detections)
+            drawBox(detections)
+        }
+    }
+    faceapi.detect(gotResults)
+}
+
+function drawBox(detections){
+    for(let i = 0; i < detections.length; i++){
+        const alignedRect = detections[i].alignedRect;
+        const x = alignedRect._box._x
+        const y = alignedRect._box._y
+        const boxWidth = alignedRect._box._width
+        const boxHeight  = alignedRect._box._height
+        
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+        rect(x, y, boxWidth, boxHeight);
+    
+        // cutout the detected face box and take average value
+
+        video.loadPixels();
+        cutout = video.get(x+trim, y+trim, boxWidth-trim*2, boxHeight-trim*2);
+        cutout.loadPixels();
+        let sumPix = 0;
+        for (let i = 0; i < cutout.pixels.length; i += 4) {
+            sumPix += (cutout.pixels[i+0] + cutout.pixels[i+1] + cutout.pixels[i+2]);
+        }
+        avgPix = sumPix / cutout.pixels.length;
+        console.log(avgPix)
+        allAvg.push(avgPix);
+        image(cutout, 0, 0);
+
+        // print avg luminance as point on lower section
+        stroke(255);
+        plotPoint = map(avgPix, 50, 100, h, h*2);
+        point(dCount, plotPoint);
+        dCount += 1;
+        if (dCount > width) {
+            dCount = 1;
+            background(0);
+        }
+
+
+    }
+    
 }
